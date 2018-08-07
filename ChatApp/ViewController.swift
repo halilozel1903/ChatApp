7316e6ad6f4e481dca78eb40968dd6b2f3a1450e
Halil Özel <halilozel1903@gmail.com>
2018-08-08 01:17:20 +0300

    mesajla ilgili özellikler eklendi.

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -10,17 +10,47 @@ import UIKit
 import JSQMessagesViewController
 
 class ViewController: JSQMessagesViewController {
+    
+    var messages = [JSQMessage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // chat arayüzü tanımlandı.
+        senderId = "1"
+        senderDisplayName = "Halil Ozel"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count // mesaj değeri kadar dön
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item] // herbir hücredeki değeri döndürür.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // mesajların renklerini ayarlama kısmı
+    
+    // lazy :  sadece kullanıldığı zaman çalıştırılacak
+    
+    lazy var outgoingBubble : JSQMessagesBubbleImage = {
+        
+        return JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleGreen())
+    }()
+    
+    lazy var incomingBubble : JSQMessagesBubbleImage = {
+        
+        return JSQMessagesBubbleImageFactory().incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }()
+    
+    
+    // mesajın gönderilme biçimi ayarlanıyor.
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        <#code#>
     }
-
-
+    
+   
 }
 
